0b3644f337868586cf3933a441c969e3c5176f3d
houssamBouazzaoui <60927878+houssamBouazzaoui@users.noreply.github.com>
2021-12-28 00:32:00 +0100

    begin project aangemaakt

---
+++ Food/Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by Houssam Bouazzaoui on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
